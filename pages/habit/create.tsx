c586a7710a3f1a63189c80db4e1a083b7c0b8562
Ku Hui Sien <ku.hui.sien@ihis.com.sg>
2021-06-26 17:49:23 +0800

    addHabit API integration

--- pages/habit/create.tsx
+++ pages/habit/create.tsx
@@ -1,34 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/client";
-import { Form } from "antd";
+import { useRouter } from "next/router";
+import { Form, Typography } from "antd";
 import { PATHS } from "lib/nav/routes";
 import classes from "styles/HabitCreate.module.css";
-import SimpleButton from "components/Buttons/SimpleButton/SimpleButton";
-import HabitFormField from "components/Habit/HabitFormField/HabitFormField";
+import HabitFormField, {
+  formFieldNames,
+} from "components/Habit/HabitFormField/HabitFormField";
+import { addHabit } from "lib/api/client/habit/AddHabit/addHabit";
+import SubmitButton from "components/Buttons/SubmitButton/SubmitButton";
 
 const Create = () => {
+  const router = useRouter();
+
   const [form] = Form.useForm();
 
-  const onFinish = async (values: any) => {
-    console.log(values);
-  };
+  // States for the form submission API call
+  const [submitIsLoading, setSubmitIsLoading] = useState(false);
+  const [submitErrorMessage, setSubmitErrorMessage] = useState<string | null>(
+    null
+  );
 
-  const onChange = (changedValues: any, values: any) => {
-    console.log(values);
+  const onFinish = async (values: any) => {
+    setSubmitIsLoading(true);
+
+    try {
+      const taskTitle = values[formFieldNames.taskTitle];
+      const notes = values[formFieldNames.notes];
+      const difficulty = values[formFieldNames.difficulty];
+
+      await addHabit({
+        taskTitle,
+        notes,
+        difficultyId: difficulty,
+      });
+
+      router.replace(PATHS.HABIT.path);
+    } catch (error) {
+      setSubmitErrorMessage(error.message);
+    } finally {
+      setSubmitIsLoading(false);
+    }
   };
 
   return (
     <div className={classes.container}>
-      <Form
-        form={form}
-        onFinish={onFinish}
-        onValuesChange={onChange}
-        onFinishFailed={onFinish}
-      >
+      <Form form={form} onFinish={onFinish}>
         <HabitFormField />
 
-        <SimpleButton htmlType="submit">CREATE</SimpleButton>
+        <SubmitButton loading={submitIsLoading}>CREATE</SubmitButton>
+
+        {submitErrorMessage && (
+          <Typography.Text type="danger">{submitErrorMessage}</Typography.Text>
+        )}
       </Form>
     </div>
   );
